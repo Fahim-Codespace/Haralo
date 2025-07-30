9966f5f66181335a24d8379f89c3751f81028e87
MD Jaliz Mahamud Mridul <139718645+moonstruck2003@users.noreply.github.com>
2025-07-30 19:13:42 +0600

    Front Page Adjustment (design)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,25 @@
-// src/pages/Home.js
 import React from 'react';
 import Navigation from '../components/navigation';
 import Footer from '../components/footer';
+import styles from '../css/homeCSS.module.css';
 
 function Home() {
   return (
-    <div className="d-flex flex-column min-vh-100">
+    <div className={styles.pageContainer}>
       <Navigation />
-
-      {/* Main content area */}
-      <main className="flex-grow-1">
-        <div className="container mt-4">
-          <div className="text-center">
+      
+      <div className={styles.gradientArea}>
+        <div className={styles.centeredContent}>
+          <div className={styles.textSection}>
             <h1>Welcome to Lost and Found</h1>
-            <p className="lead">Help reunite people with their lost belongings</p>
+            <p>Help reunite people with their lost belongings</p>
+          </div>
+          <div className={styles.buttonsSection}>
+            <button className={`${styles.actionBtn} ${styles.lostBtn}`}>Lost </button>
+            <button className={`${styles.actionBtn} ${styles.foundBtn}`}>Found</button>
           </div>
         </div>
-      </main>
+      </div>
 
       <Footer />
     </div>
