be741af6c032eddf51526ca1387180a921dd8491
MD Jaliz Mahamud Mridul <139718645+moonstruck2003@users.noreply.github.com>
2025-07-31 00:12:28 +0600

    conflict fix

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AboutUs from './pages/AboutUs';
 import Home from './pages/Home';
-import Lost from './pages/Lost';
-import Found from './pages/Found';
 import Navigation from './components/navigation';
 
 function App() {
@@ -12,8 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/lost" element={<Lost />} />
-        <Route path="/found" element={<Found />} />
       </Routes>
     </Router>
   );
