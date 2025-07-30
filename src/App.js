a8bbd12063a224938b29d202ac641e693b433e55
MD Jaliz Mahamud Mridul <139718645+moonstruck2003@users.noreply.github.com>
2025-07-31 00:20:28 +0600

    MergeConflict Fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import AboutUs from './pages/AboutUs';
 import Home from './pages/Home';
 import SignUP from './pages/SignUp';
 import Navigation from './components/navigation';
+import Lost from './pages/Lost';
+import Found from './pages/Found';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/sign-up" element={<SignUP />} />
+        <Route path="/lost" element={<Lost />} />
+        <Route path="/found" element={<Found />} />
       </Routes>
     </Router>
   );
