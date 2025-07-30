93d3fb15613050f48cb2c664c81d5260be34870c
Shahriar Mahir <shahriarmahir114@gmail.com>
2025-07-30 20:18:47 +0600

    About us final

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,9 +4,9 @@ import Footer from '../components/footer';
 import '../css/AboutUs.css';
 
 const teamMembers = [
-  { name: 'Mridul', img: '/images/mridul.jpg' },
-  { name: 'Fahim', img: '/images/fahim.jpg' },
-  { name: 'Mahir', img: '/images/mahir.jpg' },
+  { name: 'Jaliz Mahamud Mridul', img: '/images/mridul.jpg' },
+  { name: 'Md. Fahim Imam', img: '/images/fahim.jpg' },
+  { name: 'Shahriar Mahir', img: '/images/mahir.jpg' },
 ];
 
 function AboutUs() {
@@ -25,7 +25,10 @@ function AboutUs() {
         </div>
         <div className="goal">
           <h2>Our Goals</h2>
-          <p>
+          <p>Our goal is to create a platform that connects people who have lost items with those who have found them, making it easier to reunite lost belongings with their owners.
+          We aim to foster a community of trust and support, where individuals can help each other in times of need, ensuring that lost items find their way back home.
+          Through our platform, we strive to reduce the stress and frustration associated with losing personal belongings, providing a simple and effective solution for both finders and owners.
+          We believe in the power of community and aim to build a network where everyone can contribute to helping others, making the world a little bit better, one lost item at a time.
           </p>
         </div>
       </div>
