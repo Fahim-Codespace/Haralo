f813c01711387f95638c26107abddd17ecf9143d
MD Jaliz Mahamud Mridul <139718645+moonstruck2003@users.noreply.github.com>
2025-09-12 19:36:20 +0600

    Search option removed

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -35,15 +35,7 @@ const Navigation = () => {
                             
                             {/* Signup option removed from navbar */}
                         </Nav>
-                        <Form className="d-flex">
-                            <Form.Control
-                                type="search"
-                                placeholder="Search"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
+                        
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
