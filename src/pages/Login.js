7a4d4e27f4c0aff8812987612a93e3088c73c85f
Fahim <fahimimam786@gmail.com>
2025-10-01 22:09:13 +0600

    Fix: single axios instance + frontend API integration fixes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,6 +6,12 @@ import Footer from "../components/footer";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import styles from "../css/SignUP.module.css";
+import { api } from './api';
+
+export const post = (path, data, opts = {}) => api.post(path, data, opts);
+export const get = (path, opts = {}) => api.get(path, opts);
+export const put = (path, data, opts = {}) => api.put(path, data, opts);
+export const del = (path, opts = {}) => api.delete(path, opts);
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
